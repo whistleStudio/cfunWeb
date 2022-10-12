974ff02be0706ad5ad7b0cd4431621361ba62900
whistleStudio <whistleStudio@126.com>
2022-10-12 09:29:48 +0800

    202210120929

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,9 +16,19 @@ export default {
 
     }
   },
+  computed: {
+    isMobilePhone() {
+      const ua = navigator.userAgent.toLowerCase();
+      const t1 = /android|webos|iphone|ipod|blackberry|iemobile|opera mini/i.test(ua);
+      return t1 ;
+    }
+  },
   components: {
     "p-navbar": PNavbar,
     "p-footbar": PFootbar
+  },
+  created () {
+    if (this.isMobilePhone) window.location="https://dictwx.cfunworld.com"
   }
 }
 </script>
